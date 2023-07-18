2d794c3abd3a917afccd2db02412fdd964641a29
Kevin Alcocer <104943152+kevinalcocer@users.noreply.github.com>
2023-07-18 17:42:08 -0500

    index.js

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-      event.preventDefault();
-      
-      var nameInput = document.getElementById('name');
-      var emailInput = document.getElementById('email');
-      var feedbackInput = document.getElementById('feedback');
-      var errorLabel = document.getElementById('errorLabel');
-      
-      // Validar campos requeridos
-      if (nameInput.checkValidity() && emailInput.checkValidity() && feedbackInput.checkValidity()) {
-        // Enviar formulario si los campos están completos
-        errorLabel.textContent = '';
-        // Aquí puedes agregar el código para enviar los datos del formulario a un servidor o realizar alguna otra acción.
-        alert('Formulario enviado correctamente');
-        // Restablecer los campos del formulario
-        nameInput.value = '';
-        emailInput.value = '';
-        feedbackInput.value = '';
-      } else {
-        errorLabel.textContent = 'Por favor, complete todos los campos.';
-      }
-    });
+    event.preventDefault();
+    var nameInput = document.getElementById('name');
+    var emailInput = document.getElementById('email');
+    var feedbackInput = document.getElementById('feedback');
+    var errorLabel = document.getElementById('errorLabel');
+    
+    // Validar campos requeridos
+    if (nameInput.checkValidity() && emailInput.checkValidity() && feedbackInput.checkValidity()) {
+      // Enviar formulario si los campos están completos
+      errorLabel.textContent = '';
+      // Aquí puedes agregar el código para enviar los datos del formulario a un servidor o realizar alguna otra acción.
+      alert('Formulario enviado correctamente');
+      // Restablecer los campos del formulario
+      nameInput.value = '';
+      emailInput.value = '';
+      feedbackInput.value = '';
+    } else {
+      errorLabel.textContent = 'Por favor, complete todos los campos.';
+    }
+  });
