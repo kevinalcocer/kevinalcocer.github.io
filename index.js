3aec01e3be02941caaa2e48c3a4667ebd8c3e0f2
Kevin Alcocer <104943152+kevinalcocer@users.noreply.github.com>
2023-07-18 17:16:50 -0500

    index.js

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-const form = document.getElementById('contactForm');
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const feedbackInput = document.getElementById('feedback');
-const errorLabel = document.getElementById('errorLabel');
-
- form.addEventListener('submit', function(event) {
-
-   event.preventDefault();
-
-   if (!validaCamposRequeridos()) {
-    muestraMensajeError('Por favor complete todos los campos');
-  } else if (!validaEmail(emailInput.value)) { 
-    muestraMensajeError('El email no es válido');
-  } else {
-    enviaFormulario();  
-  }
-
-});
-
-
-function validaCamposRequeridos() {
- }
-
-function validaEmail(email) {
- }
-
-function muestraMensajeError(mensaje) {
-  errorLabel.textContent = mensaje;
-}
-
-function enviaFormulario() {
- }
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      
+      var nameInput = document.getElementById('name');
+      var emailInput = document.getElementById('email');
+      var feedbackInput = document.getElementById('feedback');
+      var errorLabel = document.getElementById('errorLabel');
+      
+      // Validar campos requeridos
+      if (nameInput.checkValidity() && emailInput.checkValidity() && feedbackInput.checkValidity()) {
+        // Enviar formulario si los campos están completos
+        errorLabel.textContent = '';
+        // Aquí puedes agregar el código para enviar los datos del formulario a un servidor o realizar alguna otra acción.
+        alert('Formulario enviado correctamente');
+        // Restablecer los campos del formulario
+        nameInput.value = '';
+        emailInput.value = '';
+        feedbackInput.value = '';
+      } else {
+        errorLabel.textContent = 'Por favor, complete todos los campos.';
+      }
+    });
